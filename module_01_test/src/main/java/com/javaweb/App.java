41b6cb4e8e683692417f2d3516ddc2da8e9c7499
Ezio1997-K <2911019397@qq.com>
2024-10-30 11:35:01 +0800

    这只是一个测试

---
+++ module_01_test/src/main/java/com/javaweb/App.java
@@ -0,0 +1,15 @@
+package com.javaweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        String ste = "dfafd";
+        if (ste == null) {
+
+        }
+    }
+}
