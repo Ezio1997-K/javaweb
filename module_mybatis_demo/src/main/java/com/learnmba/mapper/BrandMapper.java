f4495eb98c6ae90ba22d662f7a1be7e21ce5b43c
Ezio1997-K <2911019397@qq.com>
2024-11-01 03:09:11 +0800

    测试报错修改

---
+++ module_mybatis_demo/src/main/java/com/learnmba/mapper/BrandMapper.java
@@ -0,0 +1,15 @@
+package com.learnmba.mapper;
+
+import com.learnmba.pojo.Brand;
+
+import java.util.List;
+
+/**
+ * ClassName:BrandMapper
+ * Package:com.learnmba.mapper
+ * Description:
+ *
+ */
+public interface BrandMapper {
+    List<Brand> selectAll();
+}
--- module_mybatis_demo/src/test/java/com/javaweb/AppTest.java
+++ module_mybatis_demo/src/test/java/com/javaweb/AppTest.java
@@ -1,6 +1,8 @@
 package com.javaweb;
 
+import com.learnmba.mapper.BrandMapper;
 import com.learnmba.mapper.UserMapper;
+import com.learnmba.pojo.Brand;
 import com.learnmba.pojo.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -41,6 +43,23 @@ public class AppTest {
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         List<User> users = userMapper.selectUser();
         System.out.println(users);
+        sqlSession.close();
+    }
+
+    @Test
+    public void testBrand1() throws IOException {
+        //1.获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        //2.获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //3.获取BrandMapper接口的代理对象
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+        //4.执行查询所有方法
+        List<Brand> brands = brandMapper.selectAll();
+        brands.forEach(System.out::println);
+        //5.释放资源
         sqlSession.close();
     }
 }
