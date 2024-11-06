a7038ad95c5acfc4f520e30aaf469c15f227e7e6
Ezio1997-K <2911019397@qq.com>
2024-11-06 12:00:44 +0800

    引入service业务层

---
+++ module_web_03/src/main/java/com/learnfruit/fruit/service/impl/FruitServiceImpl.java
@@ -0,0 +1,51 @@
+package com.learnfruit.fruit.service.impl;
+
+import com.learnfruit.fruit.dao.FruitDAO;
+import com.learnfruit.fruit.dao.impl.FruitDAOImpl;
+import com.learnfruit.fruit.pojo.Fruit;
+import com.learnfruit.fruit.service.FruitService;
+
+import java.util.List;
+
+/**
+ * ClassName:FruitServiceImpl
+ * Package:com.learnfruit.fruit.service.impl
+ * Description:
+ *
+ */
+public class FruitServiceImpl implements FruitService {
+    FruitDAO fruitDAO = new FruitDAOImpl();
+
+    @Override
+    public List<Fruit> getFruitList(String keyword, Integer pageNo) {
+        return fruitDAO.getFruitList(keyword, pageNo);
+    }
+
+    @Override
+    public void addFruit(Fruit fruit) {
+        fruitDAO.addFruit(fruit);
+    }
+
+    @Override
+    public Fruit getFruitByFid(Integer fid) {
+        return fruitDAO.getFruitByFid(fid);
+    }
+
+    @Override
+    public void delFruit(Integer fid) {
+        fruitDAO.delFruit(fid);
+    }
+
+    @Override
+    public Integer getPageCount(String keyword) {
+        int fruitCount = fruitDAO.getFruitCount(keyword);
+        //总页数
+        int pageCount = (fruitCount+5-1)/5 ;
+        return pageCount;
+    }
+
+    @Override
+    public void updateFruit(Fruit fruit) {
+        fruitDAO.updateFruit(fruit);
+    }
+}
